b7f8133a6edc85ca03791b3a04245831dd8a25b6
Bill Wake <william.wake@acm.org>
2022-05-08 21:41:14 -0500

    initial project

---
+++ bassi/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  bassi
+//
+//  Created by Bill Wake on 5/8/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ bassi/bassiApp.swift
@@ -0,0 +1,17 @@
+//
+//  bassiApp.swift
+//  bassi
+//
+//  Created by Bill Wake on 5/8/22.
+//
+
+import SwiftUI
+
+@main
+struct bassiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
