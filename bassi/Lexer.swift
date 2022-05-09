aca2739a198f65cf79e6fa9b9b1e16af8316b85f
Bill Wake <william.wake@acm.org>
2022-05-09 10:42:19 -0500

    Extracted matchWhile() to pull out strings

--- bassi/Lexer.swift
+++ bassi/Lexer.swift
@@ -21,39 +21,37 @@ class Lexer {
     self.program = program
   }
 
+  func matchWhile(_ low: Character, _ high: Character) -> String {
+    let startingIndex = program.index(program.startIndex, offsetBy: index)
+
+    while program[index] >= low && program[index] <= high {
+      index += 1
+    }
+
+    let endingIndex = program.index(program.startIndex, offsetBy: index)
+    let value = program[startingIndex..<endingIndex]
+    return String(value)
+  }
+
   func next() -> Token {
     while program[index] == " " {
       index += 1
     }
 
     switch program[index] {
-    case "0", "1", "2", "3", "4", "5", "6", "7", "8", "9":
-      let startingIndex = program.index(program.startIndex, offsetBy: index)
-
-      while program[index] >= "0" && program[index] <= "9" {
-        index += 1
-      }
-
-      let endingIndex = program.index(program.startIndex, offsetBy: index)
-      let value = program[startingIndex..<endingIndex]
+    case "0", "1", "2", "3", "4",
+      "5", "6", "7", "8", "9":
+      let value = matchWhile("0", "9")
       return Token.line(Int(value)!)
 
-      case "A", "B", "C", "D", "E", "F",
+    case "A", "B", "C", "D", "E", "F",
       "G", "H", "I", "J", "K", "L",
       "M", "N", "O", "P", "Q", "R", "S",
       "T", "U", "V", "W", "X", "Y", "Z":
 
-      let startingIndex = program.index(program.startIndex, offsetBy: index)
-
-      while program[index] >= "A" && program[index] <= "Z" {
-        index += 1
-      }
-
-      let endingIndex = program.index(program.startIndex, offsetBy: index)
-      let value = program[startingIndex..<endingIndex]
-
+      let value = matchWhile("A", "Z")
+      
       if (value .starts(with: "REM")) {
-
         return Token.remark
       }
 
