6e5aafb645a99ea51fa68d0cddbabbe9ab8c1685
Bill Wake <william.wake@acm.org>
2022-05-13 11:07:32 -0500

    Added Program as model for UI - saves lines by line number, overwrites duplicates, removes number-only lines

---
+++ bassi/REPL/Program.swift
@@ -0,0 +1,25 @@
+//
+//  Program.swift
+//  bassi
+//
+//  Created by Bill Wake on 5/13/22.
+//
+
+import Foundation
+
+class Program {
+  var program : [String: String] = [:]
+
+  subscript(_ lineNumber: String) -> String {
+    get { program[lineNumber] ?? "" }
+    set {
+      if Int(newValue) == nil {
+        program[lineNumber] = newValue
+        return
+      }
+      program.removeValue(forKey: newValue)
+    }
+  }
+
+
+}
---
+++ bassiTests/REPLTests.swift
@@ -0,0 +1,37 @@
+//
+//  REPLTests.swift
+//  bassiTests
+//
+//  Created by Bill Wake on 5/13/22.
+//
+
+import XCTest
+@testable import bassi
+
+class REPLTests: XCTestCase {
+
+  func testAddingLineSavesIt() {
+    let program = Program()
+    program["10"] = "10 PRINT 42"
+    XCTAssertEqual(program["10"], "10 PRINT 42")
+  }
+  
+    func testAddingSameLineNumberOverwrites() throws {
+      let program = Program()
+      program["10"] = "10 PRINT 42"
+      program["10"] = "10 PRINT 43"
+      XCTAssertEqual(program["10"], "10 PRINT 43")
+    }
+
+  func testNonExistentLinesAreEmpty() {
+    let program = Program()
+    XCTAssertEqual(program["20"], "")
+  }
+
+  func testJustLineNumberErasesEntry() {
+    let program = Program()
+    program["25"] = "25 PRINT"
+    program["25"] = "25"
+    XCTAssertEqual(program["25"], "")
+  }
+}
