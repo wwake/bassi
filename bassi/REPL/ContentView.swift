434cbb6fb201ceb45c89cc95e8d126fb5963357d
Bill Wake <william.wake@acm.org>
2022-06-27 09:20:41 -0500

    Added tabs to repl; added RUN button to first tab

--- bassi/REPL/ContentView.swift
+++ bassi/REPL/ContentView.swift
@@ -15,8 +15,8 @@ struct ContentView: View {
 
   var repl = Repl()
 
-  var body: some View {
-    VStack {
+  fileprivate func codeView() -> some View {
+    return VStack {
       ScrollViewReader { proxy in
         ScrollView {
           Text(output.output)
@@ -32,12 +32,64 @@ struct ContentView: View {
         }
       }
 
-      TextField("text", text: $command)
+      TextField("Enter line, re-type it, or just type line number to delete it", text: $command)
         .padding()
         .onSubmit({
           repl.execute(command, output)
           command = ""
         })
+
+      HStack {
+        Spacer()
+        Button("RUN") {
+          repl.doRun(output)
+        }
+        Spacer()
+      }
+
+      Spacer()
+    }
+  }
+
+  fileprivate func runView() -> some View {
+    return ScrollViewReader { proxy in
+      ScrollView {
+        Text(output.output)
+          .font(.system(size:18, design:.monospaced))
+          .padding(.all)
+          .frame(maxWidth: .infinity, alignment: .leading)
+        Text("")
+          .id(bottom)
+      }
+      .onChange(of: output) { _ in
+        print("output changed")
+        proxy.scrollTo(bottom)
+      }
+    }
+  }
+
+  fileprivate func variableView() -> some View {
+    Text("All the variables")
+  }
+
+  var body: some View {
+    TabView {
+      codeView()
+        .tabItem {
+          Image(systemName:"curlybraces.square")
+          Text("Code")
+        }
+      runView()
+        .tabItem {
+          Image(systemName:"note.text")
+          Text("Output")
+        }
+      variableView()
+        .tabItem {
+          Image(systemName:"eye")
+          Text("Variables")
+        }
+
     }
     .frame(width: 600, height: 800)
   }
