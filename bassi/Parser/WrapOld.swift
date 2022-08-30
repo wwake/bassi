d7bb19b3420ebb75e2ea514082dc0bc682efdbb6
Bill Wake <william.wake@acm.org>
2022-08-30 12:27:33 -0500

    Extract WrapOld to production code (from test)

---
+++ bassi/Parser/WrapOld.swift
@@ -0,0 +1,38 @@
+//
+//  WrapOld.swift
+//  bassiTests
+//
+//  Created by Bill Wake on 8/30/22.
+//
+
+import Foundation
+import pcombo
+
+class WrapOld<TheTarget>: Parser {
+  typealias Input = Token
+  typealias Target = TheTarget
+
+  let basicParser: BasicParser
+  let oldParser : () throws -> Target
+
+  init(_ basicParser: BasicParser, _ oldParser : @escaping () throws -> Target) {
+    self.basicParser = basicParser
+    self.oldParser = oldParser
+  }
+
+  func parse(_ input: ArraySlice<Token>) -> ParseResult<Token, Target> {
+    let originalIndex = basicParser.tokenIndex
+    defer {
+      basicParser.tokenIndex = originalIndex
+    }
+
+    do {
+      let result = try oldParser()
+      return ParseResult.success(result, basicParser.tokens[basicParser.tokenIndex...])
+    } catch ParseError.error(let token, let message) {
+      return .failure(basicParser.indexOf(token), message)
+    } catch {
+      return .failure(0, "can't happen \(error)")
+    }
+  }
+}
--- bassiTests/WrapperTests.swift
+++ bassiTests/WrapperTests.swift
@@ -3,36 +3,6 @@ import XCTest
 import pcombo
 
 final class WrapperTests: XCTestCase {
-
-  class WrapOld<TheTarget>: Parser {
-    typealias Input = Token
-    typealias Target = TheTarget
-
-    let basicParser: BasicParser
-    let oldParser : () throws -> Target
-
-    init(_ basicParser: BasicParser, _ oldParser : @escaping () throws -> Target) {
-      self.basicParser = basicParser
-      self.oldParser = oldParser
-    }
-
-    func parse(_ input: ArraySlice<Token>) -> ParseResult<Token, Target> {
-      let originalIndex = basicParser.tokenIndex
-      defer {
-        basicParser.tokenIndex = originalIndex
-      }
-
-      do {
-        let result = try oldParser()
-        return ParseResult.success(result, basicParser.tokens[basicParser.tokenIndex...])
-      } catch ParseError.error(let token, let message) {
-        return .failure(basicParser.indexOf(token), message)
-      } catch {
-        return .failure(0, "can't happen \(error)")
-      }
-    }
-  }
-
   func test_WrapOldWithSuccessfulParse() throws {
     let basicParser = BasicParser(Lexer("RETURN"))
     XCTAssertEqual(basicParser.tokenIndex, 0)
