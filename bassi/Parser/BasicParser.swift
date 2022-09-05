cd3c15f64e1ae7ea2ddec8ca7c6c7d358c3ea139
Bill Wake <william.wake@acm.org>
2022-09-05 12:32:42 -0500

    Deleted functions already moved to StatementParser

--- bassi/Parser/BasicParser.swift
+++ bassi/Parser/BasicParser.swift
@@ -42,244 +42,16 @@ public class BasicParser : Parsing {
     }
   }
 
+  func parse() -> Parse {
+    return singleLine()
+  }
+
   func when(_ tokenType: TokenType) -> peek<satisfy<Token>> {
     peek(match(tokenType))
   }
 
-  fileprivate func makeDefStatement(_ nameParameterExpr: ((Token, String), Expression), _ remaining: ArraySlice<Token>) -> ParseResult<Token, Statement> {
-    let ((token, name), expr) = nameParameterExpr
-
-    guard token.string.count == 1 else {
-      return .failure(indexOf(token), "DEF function name cannot be followed by extra letters")
-    }
-
-    let statement = Statement.def(
-      "FN"+token.string,
-      name,
-      expr,
-      .function([.number], .number))
-
-    return .success(statement, remaining)
-  }
-
   fileprivate func makeExpressionParser() -> Bind<Token, Expression> {
     return Bind<Token, Expression>(WrapOld(self, expression).parse)
-  }
-
-  fileprivate func makeStatementsParser() -> Bind<Token, [Statement]> {
-    let statementsParser = Bind<Token, [Statement]>()
-
-    let variableParser =
-    match(.variable, "Expected variable")
-    <&> <?>(
-      match(.leftParend, "Missing '('")
-      &> expressionParser <&& match(.comma)
-      <& match(.rightParend, "Missing ')'")
-    )
-    |> { (variableToken, exprs) -> Expression in
-      let name = variableToken.string!
-
-      let type : `Type` =
-      name.last! == "$" ? .string : .number
-
-      if (exprs == nil) {
-        return .variable(name, type)
-      } else {
-        return .arrayAccess(name, type, exprs!)
-      }
-    }
-
-    let requiredVariableParser = match(.variable, "Variable is required") |> { $0.string! }
-
-    let assignParser =
-       variableParser
-    <& match(.equals, "Assignment is missing '='")
-    <&> expressionParser
-    |&> requireMatchingTypes
-    |> { (lhs, rhs) in Statement.assign(lhs, rhs) }
-
-    let dataParser =
-    match(.data)
-    &> match(.string, "Expected a data value") <&& match(.comma)
-    |> { tokens in tokens.map {$0.string} }
-    |> { strings in Statement.data(strings) }
-
-    let defParser =
-    match(.def)
-    &>
-    (  match(.fn, "DEF requires a name of the form FNx")
-       &> match(.variable, "DEF requires a name of the form FNx")
-       <& match(.leftParend, "Missing '('")
-       <&> requiredVariableParser
-       <& match(.rightParend, "DEF requires ')' after parameter")
-    )
-    <& match(.equals, "DEF requires '=' after parameter definition")
-    <&> (expressionParser |&> requireFloatType)
-    |&> makeDefStatement
-
-    let dim1Parser =
-       requiredVariableParser
-    <& match(.leftParend, "Missing '('")
-    <&> expressionParser <&& match(.comma)
-    <& match(.rightParend, "Missing ')'")
-    |> { (arrayName, dimensions) in
-      return DimInfo(arrayName, dimensions, self.typeFor(arrayName))
-    }
-
-    let dimParser =
-    match(.dim)
-    &> dim1Parser <&& match(.comma)
-    |> { Statement.dim($0) }
-
-    let exprThenGoto =
-    (expressionParser |&> requireFloatType)
-    <& match(.then, "Missing 'THEN'")
-    <&> match(.integer)
-    |> {(expr, token) in Statement.ifGoto(expr, LineNumber(token.float))}
-
-    let exprThenStatements =
-    (expressionParser |&> requireFloatType)
-    <& match(.then, "Missing 'THEN'")
-    <&> statementsParser
-    |> {(expr, stmts) in Statement.`if`(expr, stmts) }
-
-    let forParser =
-    match(.for)
-    &> requiredVariableParser
-    <& match(.equals, "'=' is required")
-    <&> (expressionParser |&> requireFloatType)
-    <& match(.to, "'TO' is required")
-    <&> (expressionParser |&> requireFloatType)
-    <&> <?>(
-      match(.step)
-      &> (expressionParser |&> requireFloatType)
-    )
-    |> { (varFromTo, stepOpt) -> Statement in
-      let ((variable, initial), final) = varFromTo
-
-      let step = stepOpt ?? Expression.number(1)
-      return Statement.`for`(variable, initial, final, step)
-    }
-
-    let gosubParser =
-    match(.gosub)
-    &> match(.integer, "Missing target of GOSUB")
-    |> { token in Statement.gosub(LineNumber(token.float))}
-
-    let gotoParser =
-    match(.goto)
-    &> match(.integer, "Missing target of GOTO")
-    |> { token in Statement.goto(LineNumber(token.float))}
-
-
-    let ifThenParser =
-    match(.if) &>
-    (exprThenGoto <||> exprThenStatements <%> "Numeric type is required")
-
-    let inputParser =
-    match(.input)
-    &> (<?>(match(.string) <& match(.semicolon, "? Semicolon required after prompt")))
-    <&> variableParser <&& match(.comma)
-    |> { (promptOpt, variables) -> Statement in
-      let prompt = promptOpt?.string ?? ""
-      return Statement.input(prompt, variables)
-    }
-
-    let letParser =
-    match(.let) &> (assignParser <%> "LET is missing variable to assign to")
-
-    let nextParser =
-    match(.next)
-    &> requiredVariableParser
-    |> { Statement.next($0) }
-
-    let onParser =
-    match(.on)
-    &> expressionParser
-    <&> (match(.goto) <|> match(.gosub) <%> "ON statement requires GOTO or GOSUB")
-    <&> (
-      match(.integer, "ON requires a comma-separated list of line numbers")
-      <&& match(.comma)
-      |> { tokens in tokens.map {LineNumber($0.float)} }
-    )
-    |> { (exprGo, lineNumbers) -> Statement in
-      let (expr, savedToken) = exprGo
-      if savedToken.type == .goto {
-        return .onGoto(expr, lineNumbers)
-      } else {
-        return .onGosub(expr, lineNumbers)
-      }
-    }
-
-    let printParser =
-    match(.print)
-    &> <*>(
-      (match(.semicolon) |> { _ in Printable.thinSpace })
-      <|> (match(.comma) |> { _ in Printable.tab })
-      <|> (expressionParser |> { Printable.expr($0) })
-    )
-    |> { printables -> Statement in
-      var values = printables
-
-      let needsNewline =
-           values.count == 0
-        || values.last! != .thinSpace && values.last != .tab
-
-      if needsNewline {
-        values.append(.newline)
-      }
-
-      return Statement.print(values)
-    }
-
-    let readParser =
-       match(.read)
-    &> variableParser <&& match(.comma)
-    |> { exprs in Statement.read(exprs) }
-
-    let statementParser =
-        assignParser
-    <|> dataParser
-    <|> defParser
-    <|> dimParser
-    <|> match(.end) |> { _ in Statement.end }
-    <|> forParser
-    <|> gosubParser
-    <|> gotoParser
-    <|> ifThenParser
-    <|> inputParser
-    <|> letParser
-    <|> nextParser
-    <|> onParser
-    <|> printParser
-    <|> readParser
-    <|> match(.remark) |> { _ in Statement.skip }
-    <|> match(.restore) |> { _ in Statement.restore }
-    <|> match(.return) |> { _ in Statement.return }
-    <|> match(.stop) |> { _ in Statement.stop }
-    <%> "Unknown statement"
-
-    let theStatementsParser =
-    statementParser <&& match(.colon, "Expected ':'")
-
-    statementsParser.bind(theStatementsParser.parse)
-    return statementsParser
-  }
-
-  fileprivate func makeSingleLineParser() -> Bind<Token, Parse> {
-    let lineParser =
-    ( match(.integer, "Line number is required")
-      |&> lineNumberInRange
-    )
-    <&> statementsParser
-    <& match(.eol, "Extra characters at end of line")
-    |> {(lineNumber, statements) in Parse(lineNumber, statements)}
-
-    return Bind<Token, Parse>(lineParser.parse)
-  }
-
-  func parse() -> Parse {
-    return singleLine()
   }
 
   func nextToken() {
@@ -329,22 +101,10 @@ public class BasicParser : Parsing {
     throw ParseError.error(token, "Missing target of GOTO")
   }
 
-  func lineNumberInRange(_ token: Token, _ remaining: ArraySlice<Token>) -> ParseResult<Token, LineNumber> {
-    let lineNumber = LineNumber(token.float)
-    if lineNumber <= 0 || lineNumber > maxLineNumber {
-      return .failure(indexOf(token), "Line number must be between 1 and \(maxLineNumber)")
-    }
-    return .success(lineNumber, remaining)
-  }
-
   // TODO - remove when WrapperTests goes away
   func recursiveDescent_exampleOfSuccessfulParse() throws -> Statement {
     nextToken()
     return .`return`
-  }
-
-  func typeFor(_ name: String) -> `Type` {
-    name.last! == "$" ? .string : .number
   }
 
   func requireFloatType(_ expr: Expression, _ remaining: ArraySlice<Token>) -> ParseResult<Token, Expression> {
